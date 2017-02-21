572c0489aaf89e5233a88ff4dd42907686c0a4cd
Madelyn Kasula <madelynkasula@gmail.com>
2017-02-20 22:01:54 -0600

    Change redirect after creating series

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,6 +1,5 @@
 class SeriesController < ApplicationController
 
-  # GET /series/new
   def new
     render 'shared/admin_only' unless is_admin?
     @series = Series.new
@@ -10,19 +9,17 @@ class SeriesController < ApplicationController
     end
   end
 
-  # POST /series
   def create
     @series = Series.new(series_params)
 
     if @series.save
-      redirect_to admins_dashboard_path, notice: 'Series was successfully created.'
+      redirect_to new_course_path, notice: 'Series successfully created.'
     else
       render :new
     end
   end
 
   private
-    # Only allow a trusted parameter "white list" through.
     def series_params
       params.require(:series).permit(:name, :end_date)
     end
