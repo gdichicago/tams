82913ac255097b5747d8e86aae7805beac66b278
Madelyn Kasula <madelynkasula@gmail.com>
2017-03-01 16:27:29 -0600

    Also forgot to update HoursHelper to remove series method

--- app/helpers/hours_helper.rb
+++ app/helpers/hours_helper.rb
@@ -1,16 +1,4 @@
 module HoursHelper
-  def build_series_hours(series, ta)
-    return false if ta.banned?
-    courses = series.courses.reject { |c| c.id == params[:course_id] }
-    courses.each do |course|
-      if course.date > Date.today
-        hour = Hour.new(course: course, teaching_assistant: ta)
-        build_hour_from(hour)
-      end
-    end
-    return true
-  end
-
   def build_hour_from(hour)
     return false if hour.teaching_assistant.banned?
     if !hour.teaching_assistant.approved?
