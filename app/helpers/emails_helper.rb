816322c4c914fb5e35369d8b385e018f001dbf57
Madelyn Kasula <madelynkasula@gmail.com>
2017-04-01 12:46:13 -0500

    Messing with images yet again

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -1,16 +1,6 @@
 module EmailsHelper
-  def update_all_email_flags(course, hours)
-    update_course_email_flag(course)
-    hours.each { |h| update_hour_email_flag(h) }
-  end
-
-  def update_course_email_flag(course)
-    course.email_sent = true
-    course.save!
-  end
-
-  def update_hour_email_flag(hour)
-    hour.email_sent = true
-    hour.save!
+  def email_image_tag(image, **options)
+    attachments[image] = File.read(Rails.root.join("app/assets/images/#{image}"))
+    image_tag attachments[image].url, **options
   end
 end
