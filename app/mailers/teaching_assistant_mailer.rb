dc582acb7ffea912dd825a2b9767328aff8bc3a2
Madelyn Kasula <madelynkasula@gmail.com>
2017-04-01 12:05:35 -0500

    Add GDI logo to email and mess with styling

--- app/mailers/teaching_assistant_mailer.rb
+++ app/mailers/teaching_assistant_mailer.rb
@@ -5,22 +5,22 @@ class TeachingAssistantMailer < ActionMailer::Base
 
   def welcome(ta)
     @ta = ta
-    mail(to: @ta['email'], subject: "#{CHAPTER_NAME}: Your TA application has been approved")
+    mail(to: @ta['email'], subject: "[#{CHAPTER_NAME}] Your TA application has been approved")
   end
 
   def pending(ta)
     @ta = ta
-    mail(to: @ta['email'], subject: "#{CHAPTER_NAME}: Your TA application has been received")
+    mail(to: @ta['email'], subject: "[#{CHAPTER_NAME}] Your TA application has been received")
   end
 
   def monthly(ta, courses, month)
     @ta = ta
     @courses = courses
-    mail(to: @ta['email'], subject: "#{CHAPTER_NAME}: Upcoming TA opportunities")
+    mail(to: @ta['email'], subject: "[#{CHAPTER_NAME}] Upcoming TA opportunities")
   end
 
   def forgot(ta)
     @ta = ta
-    mail(to: @ta['email'], subject: "#{CHAPTER_NAME}: Here's your private sign up page")
+    mail(to: @ta['email'], subject: "[#{CHAPTER_NAME}] Here's your private sign up page")
   end
 end
--- config/initializers/env_variable_config.rb
+++ config/initializers/env_variable_config.rb
@@ -2,7 +2,7 @@
 BASE_URL = ENV['BASE_URL'] || "gdichicago.herokuapp.com"
 CHAPTER_NAME = ENV['CHAPTER_NAME'] || "GDI Chicago"
 FROM_EMAIL = ENV['FROM_EMAIL'] || "chicago@girldevelopit.com"
-FROM_EMAIL_NAME = ENV["FROM_EMAIL_NAME"] || "GDI Chicago"
+FROM_EMAIL_NAME = ENV["FROM_EMAIL_NAME"] || "Girl Develop It Chicago"
 MATERIALS_URL = ENV["MATERIALS_URL"] || "gdichicago.com"
 SENDGRID_USERNAME = ENV["SENDGRID_USERNAME"]
 SENDGRID_PASSWORD = ENV["SENDGRID_PASSWORD"]
