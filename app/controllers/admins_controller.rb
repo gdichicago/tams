81be57d395161a715038ee06b2fd9a6706b8e2f5
Madelyn Kasula <madelynkasula@gmail.com>
2017-04-16 13:46:57 -0500

    Remove Active Record queries from dashboard view

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,6 +2,7 @@ class AdminsController < ApplicationController
   before_action :set_resources
 
   def dashboard
+    @prospective_tas = TeachingAssistant.prospective
     render 'shared/admin_only' unless is_admin?
   end
 end
